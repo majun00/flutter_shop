eb50b63d9c784098a5e4a42dbdfb2b14f4e6ab98
majun <needyou00@foxmail.com>
2020-03-02 19:32:08 +0800

    feat:swiper,future

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shop/service/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -12,20 +14,48 @@ class _HomePageState extends State<HomePage> {
   String homePageContent = '暂无数据';
 
   @override
-  void initState() {
-    getHomePageContent().then((val) {
-      setState(() {
-        homePageContent = val['data'].toString();
-      });
-    });
-    super.initState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('百姓生活')),
+      body: FutureBuilder(
+        future: getHomePageContent(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            var data = json.decode(snapshot.data.toString());
+            List<Map> swiper = (data['data']['slides'] as List).cast();
+            return Column(children: <Widget>[
+              SwiperDiy(
+                swiperDataList: swiper,
+              )
+            ]);
+          } else {
+            return Center(child: Text('加载中...'));
+          }
+        },
+      ),
+    );
   }
+}
+
+// 首页轮播组件
+class SwiperDiy extends StatelessWidget {
+  const SwiperDiy({Key key, this.swiperDataList}) : super(key: key);
+
+  final List swiperDataList;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('百姓生活')),
-        body: SingleChildScrollView(child: Text(homePageContent)));
+    return Container(
+      height: 333,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network("${swiperDataList[index]['image']}",
+              fit: BoxFit.fill);
+        },
+        itemCount: swiperDataList.length,
+        pagination: SwiperPagination(),
+        autoplay: true,
+      ),
+    );
   }
-  
 }

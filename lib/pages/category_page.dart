a5895e699199f66dd90d8e38b947ec5489fc04cc
majun <needyou00@foxmail.com>
2020-03-12 18:39:09 +0800

    feat:新增类别和模型

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import '../service/service_method.dart';
+import '../model/category.dart';
 
-class CategoryPage extends StatelessWidget {
+class CategoryPage extends StatefulWidget {
+  CategoryPage({Key key}) : super(key: key);
+
+  @override
+  _CategoryPageState createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('CategoryPage')));
+    _getCategory();
+    return Container(
+      child: Text('CategoryPage'),
+    );
+  }
+
+  void _getCategory() async {
+    await request('getCategory').then((val) {
+      var data = json.decode(val.toString());
+      CategoryBigListModel list = CategoryBigListModel.fromJson(data['data']);
+      list.data.forEach((item) {
+        print(item.mallCategoryName);
+      });
+    });
   }
 }

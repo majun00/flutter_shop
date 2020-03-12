f989fd253d4b0c524d7f46f438ed84876e8acde3
majun <needyou00@foxmail.com>
2020-03-12 21:58:46 +0800

    feat:新增商品分类菜单

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'dart:convert';
 import '../service/service_method.dart';
 import '../model/category.dart';
@@ -13,19 +14,75 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('商品分类'),
+      ),
+      body: Container(
+        child: Row(
+          children: <Widget>[LeftCategoryNav()],
+        ),
+      ),
+    );
+  }
+}
+
+// 左侧导航菜单
+class LeftCategoryNav extends StatefulWidget {
+  LeftCategoryNav({Key key}) : super(key: key);
+
+  @override
+  _LeftCategoryNavState createState() => _LeftCategoryNavState();
+}
+
+class _LeftCategoryNavState extends State<LeftCategoryNav> {
+  List list = [];
+
+  @override
+  void initState() {
     _getCategory();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: Text('CategoryPage'),
+      width: ScreenUtil().setWidth(180),
+      decoration: BoxDecoration(
+          border: Border(right: BorderSide(width: 1, color: Colors.black12))),
+      child: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _leftInkWell(index);
+        },
+      ),
     );
   }
 
   void _getCategory() async {
     await request('getCategory').then((val) {
       var data = json.decode(val.toString());
-      CategoryBigListModel list = CategoryBigListModel.fromJson(data['data']);
-      list.data.forEach((item) {
-        print(item.mallCategoryName);
+      CategoryModel category = CategoryModel.fromJson(data);
+      setState(() {
+        list = category.data;
       });
     });
+  }
+
+  Widget _leftInkWell(int index) {
+    return InkWell(
+        onTap: () {},
+        child: Container(
+          height: ScreenUtil().setHeight(100),
+          padding: EdgeInsets.only(left: 10, top: 20),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border:
+                  Border(bottom: BorderSide(width: 1, color: Colors.black12))),
+          child: Text(
+            list[index].mallCategoryName,
+            style: TextStyle(fontSize: ScreenUtil().setSp(28)),
+          ),
+        ));
   }
 }

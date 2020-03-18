4555d0582066414fd2093a106f2c77bde58fa059
majun <needyou00@foxmail.com>
2020-03-18 22:08:21 +0800

    feat:add empty

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -192,8 +192,12 @@ class _RightCategoryNavState extends State<RightCategoryNav> {
     request('getMallGoods', formData: data).then((val) {
       var data = json.decode(val.toString());
       CategoryGoodsListModel goodsList = CategoryGoodsListModel.fromJson(data);
-      Provide.value<CategoryGoodsListProvide>(context)
-          .getGoodsList(goodsList.data);
+      if (goodsList.data == null) {
+        Provide.value<CategoryGoodsListProvide>(context).getGoodsList([]);
+      } else {
+        Provide.value<CategoryGoodsListProvide>(context)
+            .getGoodsList(goodsList.data);
+      }
     });
   }
 }
@@ -213,16 +217,20 @@ class _CategoryGoodsListState extends State<CategoryGoodsList> {
   @override
   Widget build(BuildContext context) {
     return Provide<CategoryGoodsListProvide>(builder: (context, child, data) {
-      return Expanded(
-          child: Container(
-        width: ScreenUtil().setWidth(570),
-        child: ListView.builder(
-          itemCount: data.goodsList.length,
-          itemBuilder: (BuildContext context, int index) {
-            return _listWidget(data.goodsList, index);
-          },
-        ),
-      ));
+      if (data.goodsList.length > 0) {
+        return Expanded(
+            child: Container(
+          width: ScreenUtil().setWidth(570),
+          child: ListView.builder(
+            itemCount: data.goodsList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return _listWidget(data.goodsList, index);
+            },
+          ),
+        ));
+      } else {
+        return Text('暂时没有数据');
+      }
     });
   }
 

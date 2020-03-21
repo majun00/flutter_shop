6290eab83d89e717260f18cd07f5ef7e1ec8d536
majun <needyou00@foxmail.com>
2020-03-21 21:43:24 +0800

    feat: add shared_preferences

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,41 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:provide/provide.dart';
-import '../provide/counter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class CartPage extends StatelessWidget {
+class CartPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(child: Column(children: <Widget>[Number(), MyButton()])));
-  }
+  _CartPageState createState() => _CartPageState();
 }
 
-class Number extends StatelessWidget {
+class _CartPageState extends State<CartPage> {
+  List<String> testList = [];
+
   @override
   Widget build(BuildContext context) {
+    _show(); // 每次进入前进行显示
     return Container(
-      margin: EdgeInsets.only(top: 200),
-      child: Provide<Counter>(
-        builder: (context, child, counter) {
-          return Text(
-            '${counter.value}',
-            style: Theme.of(context).textTheme.display1,
-          );
-        },
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 500.0,
+            child: ListView.builder(
+              itemCount: testList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(testList[index]),
+                );
+              },
+            ),
+          ),
+          RaisedButton(
+            onPressed: _add,
+            child: Text('add'),
+          ),
+          RaisedButton(
+            onPressed: _clear,
+            child: Text('clear'),
+          ),
+        ],
       ),
     );
   }
-}
 
-class MyButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        child: RaisedButton(
-      onPressed: () {
-        Provide.value<Counter>(context).increment();
-      },
-      child: Text('递增'),
-    ));
+  void _add() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String temp = 'test';
+    testList.add(temp);
+    prefs.setStringList('testList', testList);
+    _show();
+  }
+
+  void _show() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      if (prefs.getStringList('testList') != null) {
+        testList = prefs.getStringList('testList');
+      }
+    });
+  }
+
+  void _clear() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    // prefs.clear();
+    prefs.remove('testList');
+    setState(() {
+      testList = [];
+    });
   }
 }
